dcfc1ac05afec558db8fa2cea664192e9a9847d0
richalsthomask <richalsthomask@gmail.com>
2024-11-22 13:08:56 +0530

    assc

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
-        Authorization: "Bearer 123456w",
+        Authorization: "Bearer 123456",
       },
     });
   };
